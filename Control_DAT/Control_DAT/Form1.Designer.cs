9f1d20a9f85500cc34de3b5803637740aa1ea9e4
Dat <datpham.utc@gmail.com>
2021-01-08 10:26:54 +0700

    CODE_WINDOW_FORM_DO_AN

---
+++ Control_DAT/Control_DAT/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace Control_DAT
+{
+    partial class DatUTC
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DatUTC));
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.cbb_COM = new System.Windows.Forms.ComboBox();
+            this.btn_connect = new System.Windows.Forms.Button();
+            this.btn_1 = new System.Windows.Forms.Button();
+            this.btn_2 = new System.Windows.Forms.Button();
+            this.btn_3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.trb_data = new System.Windows.Forms.TrackBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.serialPort2 = new System.IO.Ports.SerialPort(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.trb_data)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 115200;
+            this.serialPort1.RtsEnable = true;
+            // 
+            // cbb_COM
+            // 
+            this.cbb_COM.FormattingEnabled = true;
+            this.cbb_COM.Location = new System.Drawing.Point(75, 30);
+            this.cbb_COM.Name = "cbb_COM";
+            this.cbb_COM.Size = new System.Drawing.Size(121, 21);
+            this.cbb_COM.TabIndex = 0;
+            // 
+            // btn_connect
+            // 
+            this.btn_connect.Location = new System.Drawing.Point(75, 74);
+            this.btn_connect.Name = "btn_connect";
+            this.btn_connect.Size = new System.Drawing.Size(75, 47);
+            this.btn_connect.TabIndex = 1;
+            this.btn_connect.Text = "Connect";
+            this.btn_connect.UseVisualStyleBackColor = true;
+            this.btn_connect.Click += new System.EventHandler(this.btn_connect_Click);
+            // 
+            // btn_1
+            // 
+            this.btn_1.Location = new System.Drawing.Point(86, 104);
+            this.btn_1.Name = "btn_1";
+            this.btn_1.Size = new System.Drawing.Size(75, 23);
+            this.btn_1.TabIndex = 2;
+            this.btn_1.Text = "Light_1";
+            this.btn_1.UseVisualStyleBackColor = true;
+            this.btn_1.Click += new System.EventHandler(this.btn_1_Click);
+            // 
+            // btn_2
+            // 
+            this.btn_2.Location = new System.Drawing.Point(188, 104);
+            this.btn_2.Name = "btn_2";
+            this.btn_2.Size = new System.Drawing.Size(75, 23);
+            this.btn_2.TabIndex = 4;
+            this.btn_2.Text = "Light_2";
+            this.btn_2.UseVisualStyleBackColor = true;
+            this.btn_2.Click += new System.EventHandler(this.btn_2_Click);
+            // 
+            // btn_3
+            // 
+            this.btn_3.Location = new System.Drawing.Point(287, 104);
+            this.btn_3.Name = "btn_3";
+            this.btn_3.Size = new System.Drawing.Size(75, 23);
+            this.btn_3.TabIndex = 5;
+            this.btn_3.Text = "Group";
+            this.btn_3.UseVisualStyleBackColor = true;
+            this.btn_3.Click += new System.EventHandler(this.btn_3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "300";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(270, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Disconnect";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // trb_data
+            // 
+            this.trb_data.BackColor = System.Drawing.Color.Gray;
+            this.trb_data.Location = new System.Drawing.Point(86, 33);
+            this.trb_data.Maximum = 500;
+            this.trb_data.Minimum = 300;
+            this.trb_data.Name = "trb_data";
+            this.trb_data.Size = new System.Drawing.Size(276, 45);
+            this.trb_data.TabIndex = 8;
+            this.trb_data.Value = 300;
+            this.trb_data.Scroll += new System.EventHandler(this.trb_data_Scroll);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Image = global::Control_DAT.Properties.Resources.UTC1;
+            this.pictureBox1.Location = new System.Drawing.Point(26, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(254, 248);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.trb_data);
+            this.groupBox1.Controls.Add(this.btn_1);
+            this.groupBox1.Controls.Add(this.btn_2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btn_3);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.groupBox1.Location = new System.Drawing.Point(363, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(408, 153);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Control";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.cbb_COM);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.btn_connect);
+            this.groupBox2.Location = new System.Drawing.Point(363, 197);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(408, 144);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Connect";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(6, 287);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(290, 31);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "ĐỒ ÁN TỐT NGHIỆP";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(36, 338);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(223, 24);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "PHẠM TIẾN ĐẠT - K57";
+            // 
+            // DatUTC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 384);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "DatUTC";
+            this.Text = "Design by Dat Pham";
+            ((System.ComponentModel.ISupportInitialize)(this.trb_data)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox cbb_COM;
+        private System.Windows.Forms.Button btn_connect;
+        private System.Windows.Forms.Button btn_1;
+        private System.Windows.Forms.Button btn_2;
+        private System.Windows.Forms.Button btn_3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TrackBar trb_data;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.IO.Ports.SerialPort serialPort2;
+    }
+}
+
---
+++ Control_DAT/Control_DAT/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace Control_DAT
+{
+    public partial class DatUTC : Form
+    {   
+        public DatUTC()
+        {
+            InitializeComponent();
+        }
+        int intlen = 0;
+        byte[] Mesh_message = { 0xe8, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xff, 0xff, 0x82, 0x4c, 0xff, 0xff, 0x00 };
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private Timer _scrollingTimer = null;
+       
+
+        private void btn_connect_Click(object sender, EventArgs e)
+        {
+            if (label2.Text == "Disconnect")
+            {
+                serialPort1.PortName = cbb_COM.Text;
+                serialPort1.Open();
+                serialPort1.BaudRate = 115200;
+                serialPort1.Parity = 0 ;
+                serialPort1.StopBits = 0;
+                btn_connect.Text = "Disconnect";
+                label2.Text = "Connect";
+                cbb_COM.Enabled = false;
+            }
+            else
+            {
+                serialPort1.Close();
+                btn_connect.Text = "Connect";
+                label2.Text = "Disconnect";
+               
+                cbb_COM.Enabled = true;
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            if (intlen != ports.Length)
+            {
+                intlen = ports.Length;
+                cbb_COM.Items.Clear();
+                for (int j = 0; j < intlen; j++)
+                {
+                    cbb_COM.Items.Add(ports[j]);
+                }
+            }
+            
+        }
+
+        private void btn_1_Click(object sender, EventArgs e)
+        {
+            int Lux_data = Convert.ToInt32((label1.Text));
+
+            if(serialPort1.IsOpen == true)
+            {  
+                Mesh_message[8] = 0xa1;
+                Mesh_message[9] = 0x00;
+                Mesh_message[12] = (byte)Lux_data;
+                Mesh_message[13] = (byte)(Lux_data >> 8);
+                serialPort1.Write(Mesh_message, 0, 15);
+                
+            }
+
+        }
+
+            private void btn_2_Click(object sender, EventArgs e)
+        {
+            int Lux_data = Convert.ToInt32((label1.Text));
+
+            if (serialPort1.IsOpen == true)
+            {
+
+                Mesh_message[8] = 0xa4;
+                Mesh_message[9] = 0x00;
+                Mesh_message[12] = (byte)Lux_data;
+                Mesh_message[13] = (byte)(Lux_data >> 8);
+                serialPort1.Write(Mesh_message, 0, 15);
+
+            }
+        }
+
+        private void btn_3_Click(object sender, EventArgs e)
+        {
+            int Lux_data = Convert.ToInt32((label1.Text));
+
+            if (serialPort1.IsOpen == true)
+            {
+                Mesh_message[8] = 0xc0;
+                Mesh_message[9] = 0x01;
+                Mesh_message[12] = (byte)Lux_data;
+                Mesh_message[13] = (byte)(Lux_data >> 8);
+                serialPort1.Write(Mesh_message, 0, 15);
+
+
+            }
+        }
+
+        private void trb_data_Scroll(object sender, EventArgs e)
+        {
+            if (_scrollingTimer == null)
+            {
+
+                _scrollingTimer = new Timer()
+                {
+                    Enabled = false,
+                    Interval = 500,
+                    Tag = (sender as TrackBar).Value
+                };
+                _scrollingTimer.Tick += (s, ea) =>
+                {
+
+                    if (trb_data.Value == (int)_scrollingTimer.Tag)
+                    {
+
+                        _scrollingTimer.Stop();
+
+                        _scrollingTimer.Dispose();
+                        _scrollingTimer = null;
+                    }
+                    else
+                    {
+
+                        _scrollingTimer.Tag = trb_data.Value;
+                        label1.Text = trb_data.Value.ToString();
+                    }
+                };
+                _scrollingTimer.Start();
+            }
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
